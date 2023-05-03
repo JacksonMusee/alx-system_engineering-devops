89d0dc8445bf0543d4697fe6c1310d20f8182454
JacksonMusee <jacksonmutisya94@gmail.com>
2023-05-03 14:49:36 -0700

    Push

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\w+\s+\d+\s+\d+:\d+:\d+\s+\S+\s+\S+:\s+\d+-\d+-\d+\s+\d+:\d+:\d+\s+(?:Sent|Receive)\s+SMS\s+\[SMSC:[^\]]*\]\s+\[SVC:[^\]]*\]\s+\[ACT:[^\]]*\]\s+\[BINF:[^\]]*\]\s+\[FID:[^\]]*\]\s+\[from:(?P<sender>.*?)\]\s+\[to:(?P<receiver>.*?)\]\s+\[flags:(?P<flags>.*?)\]\s+\[msg:\d+:(?:.*?)\]\s+\[udh:\d*:\].*$/).join
+puts ARGV[0].scan(/Let Mr. Plessis do his job/).join 
