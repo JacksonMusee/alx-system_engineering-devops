4db1396e5ce7714eda0770128779af7fb12e719b
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-22 19:05:21 +0300

    Push

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 #Kills a process named killmenow
 
 exec { 'killmenow':
-    command     => '/usr/bin/pkill killmenow',
-    refreshonly => true,
-    onlyif      => '/usr/bin/pgrep killmenow'
+    command    => '/usr/bin/pkill killmenow'
 }
